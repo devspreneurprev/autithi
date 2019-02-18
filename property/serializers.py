84c44ced2b3693a6b57a29ec2895437ddec9b915
nayan32biswas <nayan32biswas@gmail.com>
2019-02-18 12:34:43 +0600

    update

--- property/serializers.py
+++ property/serializers.py
@@ -1,3 +1,1 @@
 from rest_framework.serializers import ModelSerializer
-
-from django.contrib.contenttypes.models import ContentType
--- review/serializers.py
+++ review/serializers.py
@@ -1,3 +1,2 @@
 from rest_framework.serializers import ModelSerializer
 
-from django.contrib.contenttypes.models import ContentType
