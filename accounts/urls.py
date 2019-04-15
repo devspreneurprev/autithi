556a7eab0b1f315ac76b43aa364e220d2564f47d
nayan32biswas <nayan32biswas@gmail.com>
2019-04-15 21:05:05 +0600

    add axios proparly

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('login', obtain_jwt_token),
+    path('login/', obtain_jwt_token),
     # path('login/', UserLoginAPIView.as_view(), name='login'),
     path('register/', UserRegisterAPIView.as_view(), name='register'),
 ]
