fa91fa2e80d09c2a349860c63f2bd0e193ba4f70
nayan32biswas <nayan32biswas@gmail.com>
2019-05-17 12:29:51 +0600

    add in admin

--- booking/models.py
+++ booking/models.py
@@ -10,8 +10,11 @@ class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,)
     proparty = models.ForeignKey(Proparty, on_delete=models.CASCADE,)
     requested_by_user = models.BooleanField(default=True)
-    request_accepted_by_host = models.BooleanField(default=True)
+    request_accepted_by_host = models.BooleanField(default=False)
     requested_at = models.DateField(auto_now_add=True)
     request_accepted_at = models.DateField(auto_now_add=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return "{} request to {}".format(str(self.user), str(self.host))
