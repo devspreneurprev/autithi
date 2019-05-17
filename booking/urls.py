17e451b3c393bfda180ef013b85c3f4b9f10c7f6
Mosharof <mosharof@Mosharofs-MacBook-Pro.local>
2019-05-17 14:26:28 +0600

    modified booking views

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BookingRequestAPIView
 
 urlpatterns = [
-    path('request/', BookingAPIView.as_view()),
+    path('request/', BookingRequestAPIView.as_view()),
 ]
--- booking/views.py
+++ booking/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from .models import Booking
 from property.models import Proparty
+from accounts.models import User
 
 class BookingRequestAPIView(APIView):
     # serializer_class = PropartyListSerializer
@@ -11,20 +12,26 @@ class BookingRequestAPIView(APIView):
     permission_classes = [AllowAny]
 
 
-    def get(self):
+    def get(self, request):
+        print("annonimous\n\n\n")
         queryset = Booking.objects.all()
         proparty_id = self.request.GET.get("proparty_id")
-        proparty_title = self.request.GET.get("proparty_title")
-        user = self.request.user
-
-        if user.IsAuthenticated:
-            if property_id is not None and proparty_title is not None:
-                proparty = Proparty.objects.filter(id=proparty_id, title=proparty_title)
-                if proparty.count()==1:
-                    host = proparty.host
-                    if host.count()==1
-                        booking_instance = Booking.objects.create(user=user, proparty=proparty, host=host)
-                        # here notify the host
+        # proparty_title = self.request.GET.get("proparty_title")
+        print("\n\nid ->",proparty_id, "annonimous\n\n\n")
+        user = User.objects.get(email="nayan32biswas@gmail.com")
+        print("before condition ",user, "\n\n")
+        if user is not None:
+            print("after authentication",user)
+            if proparty_id is not None:
+                proparty = Proparty.objects.get(id=proparty_id)
+                print(proparty.title,"\n\n")
+                # if proparty.count()==1:
+                print("proparty found\n\n")
+                host = proparty.host
+                print("host is -> ", host)
+                # if host.count()==1:
+                booking_instance = Booking.objects.create(user=user, proparty=proparty, host=host)
+                    # here notify the host
         else:
             # redirect to login page
             pass
