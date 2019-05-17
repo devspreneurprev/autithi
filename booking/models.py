de6257775f480df2a9a3897ae759667437d7a96e
nayan32biswas <nayan32biswas@gmail.com>
2019-05-17 14:37:38 +0600

    update booking.models

--- booking/models.py
+++ booking/models.py
@@ -10,7 +10,7 @@ class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,)
     proparty = models.ForeignKey(Proparty, on_delete=models.CASCADE,)
     requested_by_user = models.BooleanField(default=True)
-    request_accepted_by_host = models.BooleanField(default=True)
+    request_accepted_by_host = models.BooleanField(default=False)
     requested_at = models.DateField(auto_now_add=True)
     request_accepted_at = models.DateField(auto_now_add=True)
     created_at = models.DateField(auto_now_add=True)
