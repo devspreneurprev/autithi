ee2be9d2738352efbbad15930567aafe2b444dd3
nayan32biswas <nayan32biswas@gmail.com>
2019-05-17 11:56:06 +0600

    updated pycache

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from accounts.models import User, Host
+from property.models import Proparty
+# user import
+
 
 # Create your models here.
 class Booking(models.Model):
-    pass
+    host = models.ForeignKey(Host, on_delete=models.CASCADE,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    proparty = models.ForeignKey(Proparty, on_delete=models.CASCADE,)
+    requested_by_user = models.BooleanField(defautl=True)
+    request_accepted_by_host = models.BooleanField(defautl=True)
+    requested_at = models.DateField(auto_now_add=True)
+    request_accepted_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
