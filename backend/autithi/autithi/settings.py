eddd5971955f578ecdeffd76e71c0475ec0a6b4d
nayan32biswas <nayan32biswas@gmail.com>
2019-08-02 18:35:15 +0600

    update

--- backend/autithi/autithi/settings.py
+++ backend/autithi/autithi/settings.py
@@ -10,12 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-<<<<<<< HEAD
-import os, datetime
-=======
 import os
 import datetime
->>>>>>> nayan
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
