6c2723a0a59414e50a033ca13fe7e3faa17588e7
nayan32biswas <nayan32biswas@gmail.com>
2019-08-17 17:15:07 +0600

    create proparty froms and utils

---
+++ mvp/autithi/proparty/froms.py
@@ -0,0 +1,12 @@
+# from django import forms
+# from .models import Proparty, PropartyImage
+
+
+# class PropartyDetailForm(forms.ModelForm):
+#     class Meta:
+#         model = Proparty
+#         fields = [
+#             "title",
+#             "description",
+#             "house_rules",
+#         ]
