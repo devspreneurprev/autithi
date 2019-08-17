22dfc1541a20d441a299dc8f2da2ea29d80fc199
nayan32biswas <nayan32biswas@gmail.com>
2019-08-17 12:20:51 +0600

    cread urls

--- mvp/autithi/autithi/urls.py
+++ mvp/autithi/autithi/urls.py
@@ -1,4 +1,5 @@
-"""autithi URL Configuration
+"""
+autithi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('accounts.urls')),
+    path('product/', include('proparty.urls')),
 ]
