2c15cbf74f35d3356ad10b79e569f5376ec2f853
nayan32biswas <nayan32biswas@gmail.com>
2019-08-17 20:10:31 +0600

    update before add others code

--- mvp/autithi/proparty/models.py
+++ mvp/autithi/proparty/models.py
@@ -52,7 +52,7 @@ class Proparty(models.Model):
 class PropartyImage(models.Model):
     proparty = models.ForeignKey(Proparty, related_name='propartyimage', on_delete=models.CASCADE)
     caption = models.TextField()
-    image = models.ImageField(upload_to=upload_location, null=True, blank=True, )
+    image = models.ImageField(upload_to=upload_location,)
     created_at = models.DateField(auto_now_add=True,)
     updated_at = models.DateField(auto_now=True,)
 
